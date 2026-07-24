311f286fcbaa802b1a34459af2c922e5d777b323
leguru607 <190280988+leguru607@users.noreply.github.com>
2026-07-25 02:44:07 +0800

    revert: убрали pomodoro/i3blocks, простой i3status + обычный lock screen

---
+++ i3/.config/i3/scripts/lock.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+i3lock \
+    --blur=8 \
+    --bar-indicator \
+    --bar-orientation=horizontal \
+    --bar-count=20 \
+    --bar-base-width=200 \
+    --bar-max-height=40 \
+    --bar-step=4 \
+    --bar-periodic-step=1 \
+    --bar-color=444444ff \
+    --ringver-color=ffffffff \
+    --ringwrong-color=331111ff \
+    --keyhl-color=ffffffff \
+    --bshl-color=331111ff \
+    -e \
+    --pass-media-keys \
+    --pass-screen-keys
